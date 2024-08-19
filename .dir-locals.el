1e8d090ea80a4d1d1ca8f4b1ec713e1f77ade46b
John Gosset <john@gossetx.com>
2024-08-19 11:53:16 -0400

    feat: Add function to generate TOC starting with h2

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,11 @@
 ;;; Directory Local Variables            -*- no-byte-compile: t -*-
 ;;; For more information see (info "(emacs) Directory Variables")
 
-((markdown-mode . ((markdown-toc-indentation-space . 2)
-		   (markdown-toc-header-toc-title . "## Contents"))))
+((markdown-mode . ((markdown-toc-user-toc-structure-manipulation-fn . (lambda (markdown-list)
+									(let* ((remaining-list (cdr markdown-list))
+									       (processed-list (mapcar (lambda (item)
+													 (cons (1- (car item)) (cdr item)))
+												       remaining-list)))
+									  processed-list)))
+								    (markdown-toc-indentation-space . 2)
+								    (markdown-toc-header-toc-title . "## Contents"))))
